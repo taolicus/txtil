561f74f881be9cd8e45a6fa141aca401c211ecdb
Tao <taozamorano@gmail.com>
2025-01-15 23:33:20 +0000

    add layer rendering and render printing

--- txtil.js
+++ txtil.js
@@ -2,27 +2,28 @@ const pattern_bg = [
   [1,1,1],
   [1,1,1],
   [1,1,1],
-]
+];
 
 const pattern_01 = [
-  [1]
-]
+  [1],
+];
 
 const pattern_02 = [
   [1, 0],
-  [0, 1]
+  [0, 1],
 ];
 
 const layer_bg = {
-  character: [' ', '.', ',', ';', ' ', '\'', '"', '´', ' '],
-  pattern: pattern_bg
+  //character: [' ', '.', ',', ';', ' ', "'", '"', '´', ' '],
+  character: '•',
+  pattern: pattern_bg,
 };
 
 const layer_01 = {
   character: 'X',
   pattern: pattern_01,
   xOffset: 2,
-  yOffset: 2
+  yOffset: 2,
 };
 
 const layer_02 = {
@@ -30,34 +31,52 @@ const layer_02 = {
   pattern: pattern_02,
 };
 
-const w = 3//39;
-const h = 3//32;
+// const w = 3;//39;
+// const h = 3;//32;
 
-let noise = [];
+// let noise = [];
 
-for(let y = 0; y < h; y++) {
-  let row = [];
-  for(let x = 0; x < w; x++) {
-    const randomSymbol = Math.floor(Math.random() * symbols.length);
-    row.push(symbols[randomSymbol]);
-  }
-  noise.push(row);
-}
+// for(let y = 0; y < h; y++) {
+//   let row = [];
+//   for(let x = 0; x < w; x++) {
+//     const randomSymbol = Math.floor(Math.random() * symbols.length);
+//     row.push(symbols[randomSymbol]);
+//   }
+//   noise.push(row);
+// }
 
-function collapseLayers(bgLayer, fgLayer) {
-  let collapsedLayers = structuredClone(bgLayer)
-  for(let y = 0; y < collapsedLayers.length; y++) {
-    if(fgLayer[y]) {
-      for(let x = 0; x < collapsedLayers[y].length; x++) {
-        if(fgLayer[y][x]) collapsedLayers[y][x] = fgLayer[y][x]
-      }
+function collapseLayers(render, layer) {
+  for(let y = 0; y < layer.pattern.length; y++) {
+    for(let x = 0; x < layer.pattern[y].length; x++) {
+      if(layer.pattern[y][x]) render[y][x] = layer.character;
     }
   }
-  return collapsedLayers;
+  return render;
 }
 
 function renderLayer(layer) {
-  layer.forEach(y => {
+  let render = [];
+  for(let y = 0; y < layer.pattern.length; y++) {
+    render.push([]);
+    for(let x = 0; x < layer.pattern[y].length; x++) {
+      render[y].push(layer.character);
+    }
+  }
+  return render;
+}
+
+function renderComposition(composition) {
+  let first_layer = structuredClone(composition[0]);
+  let render = renderLayer(first_layer);
+  for(let l = 1; l < composition.length; l++) { // skip index 0 (bg layer)
+    const current_layer = composition[l];
+    render = collapseLayers(render, current_layer);
+  }
+  printRender(render);
+}
+
+function printRender(render) {
+  render.forEach(y => {
     y.forEach(x => {
       process.stdout.write(x);
     })
@@ -65,18 +84,10 @@ function renderLayer(layer) {
   });
 }
 
-function renderComposition(composition) {
-  let render = structuredClone(composition[0])
-  for(let l = 1; l < composition.length; l++) { // skip index 0 (bg layer)
-    render = collapseLayers(render, composition[l])
-  }
-  renderLayer(render)
-}
-
 let composition = [
-  layer_bg
-  pattern_01,
-  pattern_02,
+  layer_bg,
+  layer_02,
+  layer_01,
 ];
 
-renderComposition(composition)
+renderComposition(composition);
